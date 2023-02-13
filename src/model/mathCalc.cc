7e6c9de81b45d83799cc3dfdbffaf6fba985e772
englishk <englishk@student.21-school.ru>
2023-02-13 16:19:34 +0700

    Count ready

--- src/model/mathCalc.cc
+++ src/model/mathCalc.cc
@@ -17,28 +17,76 @@ inline const std::map<std::string, std::function<double(double)>>
 inline const std::map<std::string, std::function<double(double, double)>>
     MathCalc::kOperators_{
         {"+", [](double val1, double val2) -> double { return val1 + val2; }},
-        {"-", [](double val1, double val2) -> double { return val1 - val2; }},
-        {"/", [](double val1, double val2) -> double { return val1 / val2; }},
+        {"-", [](double val1, double val2) -> double { return val2 - val1; }},
+        {"/", [](double val1, double val2) -> double { return val2 / val1; }},
         {"*", [](double val1, double val2) -> double { return val1 * val2; }},
         {"^",
-         [](double val1, double val2) -> double { return pow(val1, val2); }},
+         [](double val1, double val2) -> double { return pow(val2, val1); }},
         {"mod",
-         [](double val1, double val2) -> double { return fmod(val1, val2); }},
-        {"A", [](double val1, double val2) -> double { return val1 - val2; }},
-        {"C", [](double val1, double val2) -> double { return val1 + val2; }}};
+         [](double val1, double val2) -> double { return fmod(val2, val1); }},
+        {"A", [](double val1, double val2) -> double { return val2 - val1; }},
+        {"C", [](double val1, double val2) -> double { return val2 + val1; }}};
 
 double MathCalc::ShuntingYard(std::string& str_from_label, double& x) {
   this->tokenizator_.ToTokens(str_from_label, x);
   this->validator_.Validation(this->tokenizator_.GetListToken());
   std::stack<double> stack_number;
-  std::stack<std::string> stack_operators;
+  std::stack<Token> stack_operations;
   auto current_token = this->tokenizator_.GetListToken().begin();
   while (current_token != this->tokenizator_.GetListToken().end()) {
     if (current_token->IsNumber()) {
       stack_number.push(std::stod(current_token->GetTokenString()));
+      ++current_token;
+    } else {
+      if (current_token->IsUnaryOperator()) {
+        stack_number.push(0.0);
+      }
+      if (!stack_operations.size()) {
+        stack_operations.push(*current_token);
+        ++current_token;
+      } else if (stack_operations.size() &&
+                 current_token->GetPriority() >
+                     stack_operations.top().GetPriority()) {
+        stack_operations.push(*current_token);
+        ++current_token;
+      } else if (stack_operations.size() &&
+                 current_token->GetPriority() <=
+                     stack_operations.top().GetPriority()) {
+        CountValue(stack_operations, stack_number);
+      }
+    }
+    if (current_token->IsOpenParenthesis() &&
+        current_token != this->tokenizator_.GetListToken().end()) {
+      stack_operations.push(*current_token);
+      ++current_token;
+    } else if (current_token->IsCloseParenthesis() &&
+               current_token != this->tokenizator_.GetListToken().end()) {
+      while (stack_operations.top().GetTokenString() != "(") {
+        CountValue(stack_operations, stack_number);
+      }
+      ++current_token;
+      stack_operations.pop();
     }
   }
-  return 0.0;
+  while (!stack_operations.empty()) {
+    CountValue(stack_operations, stack_number);
+  }
+  return stack_number.top();
+}
+
+void MathCalc::CountValue(std::stack<Token>& stack_operations,
+                          std::stack<double>& stack_number) {
+  double tmp = stack_number.top();
+  stack_number.pop();
+  if (stack_operations.top().GetType() == Token::TypeTokens::kFunction) {
+    tmp = kFunctions_.at(stack_operations.top().GetTokenString())(tmp);
+  } else {
+    tmp = kOperators_.at(stack_operations.top().GetTokenString())(
+        tmp, stack_number.top());
+    stack_number.pop();
+  }
+  stack_number.push(tmp);
+  stack_operations.pop();
 }
 
 }  // namespace s21
