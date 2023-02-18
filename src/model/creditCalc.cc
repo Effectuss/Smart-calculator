18a0f5f5c616364bbd89d3a7635b8146d0806b96
englishk <englishk@student.21-school.ru>
2023-02-18 18:59:38 +0700

    need credit

--- src/model/creditCalc.cc
+++ src/model/creditCalc.cc
@@ -1,20 +1,52 @@
 #include "creditCalc.h"
 
+#include "iostream"
+
 namespace s21 {
 
-void CreditCalc::CalculateCredit(DataForCredit &data_for_credit)
-{
+void CreditCalc::CalculateCredit(DataForCredit &data_for_credit) {
+  if (data_for_credit.type_credit == CreditCalc::TypeOfCredit::kAnnuity) {
+    this->AnnuityCredit(
+        data_for_credit.sum_credit, data_for_credit.amount_month,
+        data_for_credit.interest_rate, data_for_credit.data_for_table);
 
+  } else if (data_for_credit.type_credit ==
+             CreditCalc::TypeOfCredit::kDifferentiated) {
+    this->DifferentiatedCredit(
+        data_for_credit.sum_credit, data_for_credit.amount_month,
+        data_for_credit.interest_rate, data_for_credit.data_for_table);
+  }
 }
 
-void CreditCalc::AnnuityCredit(DataForCredit &data_for_credit)
-{
+void CreditCalc::AnnuityCredit(
+    long double &sum_credit, long double &amount_months,
+    long double &interest_rate,
+    std::vector<std::array<long double, 4>> data_for_table) {
+  long double interest_per_month = interest_rate / (100.0L * 12.0L);
+  long double pay_per_month =
+      sum_credit *
+      ((interest_rate / 100L / 12L) +
+       ((interest_rate / 100L / 12L) /
+        (pow(1L + (interest_rate / 100L / 12L), amount_months) - 1L)));
 
+  long double after_payment = sum_credit;
+
+  for (int i = 0; i < static_cast<int>(amount_months); --amount_months) {
+    std::array<double, 4> tmp;
+    data_for_table.at(i).push_back(tmp);
+    data_for_table.at(i).at(NameOfColums::kPayPerMonth) = pay_per_month;
+    data_for_table.at(i).at(NameOfColums::kInterestPay) =
+        after_payment * interest_per_month;
+    data_for_table.at(i).at(NameOfColums::kDebtPay) =
+        pay_per_month - data_for_table.at(i).at(NameOfColums::kInterestPay);
+    after_payment -= data_for_table.at(i).at(NameOfColums::kDebtPay);
+    data_for_table.at(i).at(NameOfColums::kResidue) = after_payment;
+  }
 }
 
-void CreditCalc::DifferentiatedCredit(DataForCredit &data_for_credit)
-{
-
-}
+void CreditCalc::DifferentiatedCredit(
+    long double &sum_credit, long double &amount_months,
+    long double &interest_rate,
+    std::vector<std::array<long double, 4>> data_for_table) {}
 
 }  // namespace s21
--- src/model/creditCalc.h
+++ src/model/creditCalc.h
@@ -1,25 +1,31 @@
 #ifndef __CREDITCALC_H__
 #define __CREDITCALC_H__
 #include <array>
+#include <cmath>
 #include <vector>
 namespace s21 {
 
 class CreditCalc {
  public:
   enum TypeOfCredit { kAnnuity = 1, kDifferentiated };
+  enum NameOfColums { kPayPerMonth, kDebtPay, kInterestPay, kResidue };
 
   struct DataForCredit {
-    double sum_credit;
-    double amount_month;
-    double interest_rate;
+    long double sum_credit;
+    long double amount_month;
+    long double interest_rate;
     TypeOfCredit type_credit;
-    std::vector<std::array<double, 4>> data_for_table;
+    std::vector<std::array<long double, 4>> data_for_table;
   };
 
-    void CalculateCredit(CreditCalc::DataForCredit &data_for_credit);
-    void AnnuityCredit(CreditCalc::DataForCredit &data_for_credit);
-    void DifferentiatedCredit(CreditCalc::DataForCredit &data_for_credit);
-
+  void CalculateCredit(CreditCalc::DataForCredit &data_for_credit);
+  void AnnuityCredit(long double &sum_credit, long double &amount_months,
+                     long double &interest_rate,
+                     std::vector<std::array<long double, 4>> data_for_table);
+  void DifferentiatedCredit(
+      long double &sum_credit, long double &amount_months,
+      long double &interest_rate,
+      std::vector<std::array<long double, 4>> data_for_table);
 };
 
 }  // namespace s21
