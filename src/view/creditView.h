d1bd4435e1efdc7d60b40f474ad1d6dddd4d5878
englishk <englishk@student.21-school.ru>
2023-02-19 16:32:42 +0700

    Credit is finish

--- src/view/creditView.h
+++ src/view/creditView.h
@@ -1,39 +1,40 @@
 #ifndef CREDITVIEW_H
 #define CREDITVIEW_H
 
-#include <QWidget>
 #include <controller.h>
+
+#include <QWidget>
 
 namespace Ui {
 class CreditView;
 }
 namespace s21 {
-class CreditView : public QWidget
-{
-    Q_OBJECT
+class CreditView : public QWidget {
+  Q_OBJECT
 
-public:
-    explicit CreditView(QWidget *parent = nullptr);
-    void SetController(Controller *controller);
-    void FillDataForCount(CreditCalc::DataForCredit& data_for_credit);
-    void SetDefaultSettings();
-    ~CreditView();
+ public:
+  explicit CreditView(QWidget *parent = nullptr);
+  void SetController(Controller *controller);
+  void FillDataForCount(CreditCalc::DataForCredit &data_for_credit);
+  void SetDefaultSettings();
+  void FillTheTable(CreditCalc::DataForCredit &data_for_credit);
+  ~CreditView();
 
-protected:
-    void closeEvent(QCloseEvent *event);
+ protected:
+  void closeEvent(QCloseEvent *event);
 
-signals:
-    void showParent();
+ signals:
+  void showParent();
 
-private slots:
-    void OnActionCalculateClicked();
-    void OnActionCloseClicked();
-    void OnActionClearClicked();
+ private slots:
+  void OnActionCalculateClicked();
+  void OnActionCloseClicked();
+  void OnActionClearClicked();
 
-private:
-    Ui::CreditView *ui;
-    Controller *controller_;
+ private:
+  Ui::CreditView *ui;
+  Controller *controller_;
 };
-}
+}  // namespace s21
 
-#endif // CREDITVIEW_H
+#endif  // CREDITVIEW_H
